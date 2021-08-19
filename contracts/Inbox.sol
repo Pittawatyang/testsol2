2bfacc85b996825f7bb86f6e9652abea06b2ca6c
Pittawatyang <pittawatyang@gmail.com>
2021-08-19 13:00:48 +0700

    8/19/21 13:00

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,1 +1,13 @@
+pragma solidity >=0.4.25;
 
+contract Inbox {
+    string public message;
+
+    function currentInbox(string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
---
+++ test/Inbox.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface } = require('../compile'); 
+const { bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts() //wait for account to be done
+    // await goes with async
+    // Deploy contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        // deploy byte code & pass on initial message
+        .deploy({ data: bytecode, arguments: ['Hi there!'] }) //want to deploy a new copy
+        // use account 0 and limit max gas at 1 million
+        .send({ from: accounts[0], gas: '1000000' }) //instructs web3 to send out a transaction creating contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
+
+
